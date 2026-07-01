66f94c9a494c60918f78c072dd80e04694b9e802
Jerry-Gren <jerryhuang328@gmail.com>
2026-07-02 00:32:25 +0800

    refactor(hal/x86_64): namespace isr stubs

--- hal/x86_64/idt.c
+++ hal/x86_64/idt.c
@@ -16,20 +16,24 @@ static void legacy_pic_disable(void) {
 static struct idt_descriptor idt[IDT_NR_DESCRIPTORS];
 static struct idt_ptr idtr;
 
-#define DECL_ISR(n) extern void isr##n(void);
+#define DECL_ISR(n) extern void x86_64_isr##n(void);
 DECL_ISR(0)  DECL_ISR(1)  DECL_ISR(2)  DECL_ISR(3)  DECL_ISR(4)  DECL_ISR(5)  DECL_ISR(6)  DECL_ISR(7)
 DECL_ISR(8)  DECL_ISR(9)  DECL_ISR(10) DECL_ISR(11) DECL_ISR(12) DECL_ISR(13) DECL_ISR(14) DECL_ISR(15)
 DECL_ISR(16) DECL_ISR(17) DECL_ISR(18) DECL_ISR(19) DECL_ISR(20) DECL_ISR(21) DECL_ISR(22) DECL_ISR(23)
 DECL_ISR(24) DECL_ISR(25) DECL_ISR(26) DECL_ISR(27) DECL_ISR(28) DECL_ISR(29) DECL_ISR(30) DECL_ISR(31)
 
 /* in interrupts.S */
-extern void isr_default(void);
+extern void x86_64_isr_default(void);
 
-static void *isr_stub_table[32] = {
-	isr0, isr1, isr2, isr3, isr4, isr5, isr6, isr7,
-	isr8, isr9, isr10, isr11, isr12, isr13, isr14, isr15,
-	isr16, isr17, isr18, isr19, isr20, isr21, isr22, isr23,
-	isr24, isr25, isr26, isr27, isr28, isr29, isr30, isr31
+static void *x86_64_isr_stub_table[32] = {
+	x86_64_isr0, x86_64_isr1, x86_64_isr2, x86_64_isr3,
+	x86_64_isr4, x86_64_isr5, x86_64_isr6, x86_64_isr7,
+	x86_64_isr8, x86_64_isr9, x86_64_isr10, x86_64_isr11,
+	x86_64_isr12, x86_64_isr13, x86_64_isr14, x86_64_isr15,
+	x86_64_isr16, x86_64_isr17, x86_64_isr18, x86_64_isr19,
+	x86_64_isr20, x86_64_isr21, x86_64_isr22, x86_64_isr23,
+	x86_64_isr24, x86_64_isr25, x86_64_isr26, x86_64_isr27,
+	x86_64_isr28, x86_64_isr29, x86_64_isr30, x86_64_isr31
 };
 
 static void set_idt_descriptor(uint8_t vector, uintptr_t isr, uint8_t attributes) {
@@ -47,14 +51,14 @@ void x86_64_idt_init(void) {
 	idtr.limit = sizeof(idt) - 1;
 	idtr.base  = (uint64_t)&idt;
 
-	/* fill all entries with isr_default handler */
+	/* fill all entries with default isr handler */
 	for (int i = 0; i < IDT_NR_DESCRIPTORS; i++) {
-		set_idt_descriptor(i, (uintptr_t)isr_default, IDT_ATTR(1, DPL_KERNEL, IDT_TYPE_INTR64));
+		set_idt_descriptor(i, (uintptr_t)x86_64_isr_default, IDT_ATTR(1, DPL_KERNEL, IDT_TYPE_INTR64));
 	}
 
 	/* override entries for first 32 vectors */
 	for (int i = 0; i < 32; i++) {
-		set_idt_descriptor(i, (uintptr_t)isr_stub_table[i], IDT_ATTR(1, DPL_KERNEL, IDT_TYPE_INTR64));
+		set_idt_descriptor(i, (uintptr_t)x86_64_isr_stub_table[i], IDT_ATTR(1, DPL_KERNEL, IDT_TYPE_INTR64));
 	}
 
 	/* disabling 8259 pic */
